10c9642397d310cfd740d34a12ace97ecf00940d
adchaf <146009370+adchaf@users.noreply.github.com>
2023-10-05 16:26:39 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,83 +1,32 @@
-import { useState } from "react";
-import copy from "copy-to-clipboard";
+import React from "react"; 
+import useDownloader from "react-use-downloader"; 
 
-const Index = () => {
-  const [videoURL, setVideoURL] = useState("");
-  const [thumbnailOptions, setThumbnailOptions] = useState([]);
+export default function App() { 
+const { size, elapsed, percentage, download, 
+		cancel, error, isInProgress } = 
+	useDownloader(); 
 
-  const getYouTubeThumbnail = (url) => {
-    let regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
-    let match = url.match(regExp);
+const fileUrl = "/File.pdf"; 
+const filename = "File.pdf"; 
 
-    if (match && match[1].length === 11) {
-      const videoURL = match[1];
-      const thumbnailBaseUrl = "http://img.youtube.com/vi/";
+return ( 
+	<div className="App"> 
+	<h3>GeeksforGeeks - File Downloader</h3> 
+	<p>Download is in {isInProgress ? 
+		"in progress" : "stopped"}</p> 
 
-      const options = [
-        { resolution: "HD (1280x720)", code: "maxresdefault" },
-        { resolution: "SD (640x480)", code: "sddefault" },
-        { resolution: "Normal (480x360)", code: "hqdefault" },
-        { resolution: "Medium (320x180)", code: "mqdefault" },
-        { resolution: "Low (120x90)", code: "default" },
-      ];
+	<button onClick={() => download(fileUrl, filename)}> 
+		Click to download the file 
+	</button> 
+	<button onClick={() => cancel()}> 
+		Cancel the download 
+	</button> 
+	<p>Download size in bytes {size}</p> 
 
-      const thumbnailOptions = options.map((option) => ({
-        resolution: option.resolution,
-        url: `${thumbnailBaseUrl}${videoURL}/${option.code}.jpg`,
-      }));
-
-      setThumbnailOptions(thumbnailOptions);
-      setVideoURL("");
-    } else {
-      setThumbnailOptions([]);
-    }
-  };
-
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <header className="text-center mb-8">
-        <h1 className="text-3xl font-bold mb-2">
-          Youtube Thumbnail Downloader
-        </h1>
-        <p className="text-gray-600">
-          Download high-quality thumbnails from YouTube videos.
-        </p>
-      </header>
-      <div className="text-center">
-        <input
-          type="text"
-          className="w-full md:w-1/2 px-4 py-2 border rounded"
-          placeholder="Enter YouTube URL"
-          value={videoURL}
-          onChange={(e) => setVideoURL(e.target.value)}
-        />
-        <button
-          className="btn-blue mt-2"
-          onClick={() => getYouTubeThumbnail(videoURL)}
-        >
-          Download Thumbnails
-        </button>
-      </div>
-      {thumbnailOptions.length > 0 && (
-        <div className="mt-8">
-          <h2 className="text-xl font-semibold mb-4">Thumbnail Options</h2>
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-            {thumbnailOptions.map((option, index) => (
-              <div key={index} className="thumbnail-option">
-                <img src={option.url} alt={`Thumbnail ${index + 1}`} />
-                <button
-                  className="btn-blue mt-2"
-                  onClick={() => copy(option.url)}
-                >
-                  Copy Image URL
-                </button>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Index;
+	<label for="file">Downloading progress:</label> 
+	<progress id="file" value={percentage} max="100" /> 
+	<p>Elapsed time in seconds {elapsed}</p> 
+	{error && <p>possible error {JSON.stringify(error)}</p>} 
+	</div> 
+); 
+} 
